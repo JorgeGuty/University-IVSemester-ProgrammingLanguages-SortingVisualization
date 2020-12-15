e2a572b91578d74c33aa72771f3055c7eae749a9
Sebastian <salpizar@outcoding.com>
2020-12-14 22:08:22 -0600

    Added Missing Sortings
    Added the missing sorting algorithms

--- sorting-algorithms/Algorithms/insertionSort.go
+++ sorting-algorithms/Algorithms/insertionSort.go
@@ -1,1 +1,15 @@
 package Algorithms
+
+
+func insertionsort(items []int) {
+    var n = len(items)
+    for i := 1; i < n; i++ {
+        j := i
+        for j > 0 {
+            if items[j-1] > items[j] {
+                items[j-1], items[j] = items[j], items[j-1]
+            }
+            j = j - 1
+        }
+    }
+}
--- sorting-algorithms/Algorithms/selectionSort.go
+++ sorting-algorithms/Algorithms/selectionSort.go
@@ -1,1 +1,15 @@
 package Algorithms
+
+func selectionsort(items []int) {
+    var n = len(items)
+    for i := 0; i < n; i++ {
+        var minIdx = i
+        for j := i; j < n; j++ {
+            if items[j] < items[minIdx] {
+                minIdx = j
+            }
+        }
+        items[i], items[minIdx] = items[minIdx], items[i]
+    }
+}
+
